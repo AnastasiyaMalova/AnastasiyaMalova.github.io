ba0d96bb27b0077bbe0693c7f800ffc51982cc76
Anastasiya <malova.iteb@gmail.com>
2022-09-18 16:43:45 +0300

    update 18.09

--- 2021/Pulse-project/src/js/index.js
+++ 2021/Pulse-project/src/js/index.js
@@ -1,5 +1,4 @@
 //slick slider
-// $(document).ready(function(){
 //     $('.carousel__inner').slick({
 //         speed: 1200,
 //         slidesToShow: 1,
@@ -20,64 +19,167 @@
 //     });
 //   });
 
-//tiny slider 2
-const slider = tns({
-   container: '.carousel__inner',
-   autoplay: true,
-   autoplayButtonOutput: false,
-   autoplayHoverPause: true,
-   arrowKeys: true,
-   items: 1,
-   slideBy: 'page',
-   controls: false,
-   navPosition: 'bottom',
-   responsive: {
+$(document).ready(function () {
+  //tiny slider 2
+  const slider = tns({
+    container: '.carousel__inner',
+    autoplay: true,
+    autoplayButtonOutput: false,
+    autoplayHoverPause: true,
+    arrowKeys: true,
+    items: 1,
+    slideBy: 'page',
+    controls: false,
+    navPosition: 'bottom',
+    responsive: {
       767: {
-         nav: false,
+        nav: false,
       },
-   }
-});
+    },
+  });
 
-document.querySelector('.prev').addEventListener('click', function () {
-   slider.goTo('prev');
-});
+  document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+  });
 
-document.querySelector('.next').addEventListener('click', function () {
-   slider.goTo('next');
-});
+  document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+  });
 
-$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
-   $(this)
-      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
-});
+  $('ul.catalog__tabs').on(
+    'click',
+    'li:not(.catalog__tab_active)',
+    function () {
+      $(this)
+        .addClass('catalog__tab_active')
+        .siblings()
+        .removeClass('catalog__tab_active')
+        .closest('div.container')
+        .find('div.catalog__content')
+        .removeClass('catalog__content_active')
+        .eq($(this).index())
+        .addClass('catalog__content_active');
+    }
+  );
 
-toggleSlide('.catalog-item__link');
-toggleSlide('.catalog-item__back');
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back');
 
-
-function toggleSlide(link) {
-   $(link).each(function (i) {
+  function toggleSlide(link) {
+    $(link).each(function (i) {
       $(this).on('click', function (e) {
-         e.preventDefault();
-         $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-         $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-      })
-   })
-}
+        e.preventDefault();
+        $('.catalog-item__content')
+          .eq(i)
+          .toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+      });
+    });
+  }
 
-// modal
-// выбор элементов по атрибутам
-$('[data-modal="consultation').on('click', () => {
-   $('.overlay, #consultation').fadeIn();
-});
-$('.modal__close').on('click', () => {
-   $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
-});
-$('.button_mini').each(function (i) {
-   $(this).on('click', () => {
+  // modal
+  // выбор элементов по атрибутам
+  $('[data-modal="consultation').on('click', () => {
+    $('.overlay, #consultation').fadeIn();
+  });
+  $('.modal__close').on('click', () => {
+    $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+  });
+  $('.overlay').on('click', (e) => {
+    if (e.target.classList.contains('overlay')) {
+      $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    }
+  });
+  $(document).on('keydown', (e) => {
+    if (e.keyCode == 27) {
+      $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    }
+  });
+  $('.button_mini').each(function (i) {
+    $(this).on('click', () => {
       // С помощью функции text подставляем название товара,на который мы нажали (catalog-item__subtitle) в modal__descr
-      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text()); /* eq позволяет получить определенный элемент по порядку*/
+      $('#order .modal__descr').text(
+        $('.catalog-item__subtitle').eq(i).text()
+      ); /* eq позволяет получить определенный элемент по порядку*/
       $('.overlay, #order').fadeIn('slow');
-   });
+    });
+  });
+
+  const validateForm = (form) => {
+    $(form).validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 3,
+        },
+        phone: 'required',
+        email: {
+          required: true,
+          email: true,
+        },
+      },
+      messages: {
+        name: {
+          required: 'Пожалуйста, введите свое имя',
+          minlength: jQuery.validator.format('Введите от {0}-х символов'),
+        },
+        phone: 'Пожалуйста, введите свой телефон',
+        email: {
+          required: 'Пожалуйста, введите свой почтовый адрес',
+          email: 'Почтовый адрес введен неправильно',
+        },
+      },
+    });
+  };
+  validateForm('#consultation-form');
+  validateForm('#consultation form');
+  validateForm('#order form');
+
+  $('input[name=phone]').mask('7 (999) 999-9999');
+
+  $('form').submit(function (e) {
+    e.preventDefault();
+    // Случай, если форма не прошла валидацию (пустой запрос или др.)
+    if (!$(this).valid()) {
+      return;
+    }
+
+    $.ajax({
+      type: 'POST',
+      url: './mailer/smart.php',
+      data: $(this).serialize(),
+    }).done(function () {
+      $(this).find('input').val('');
+      $('#consultation, #order').fadeOut();
+      $('.overlay, #thanks').fadeIn('slow');
+      // Обновление форм
+      $('form').trigger('reset');
+    });
+    return false;
+  });
+
+  //smooth scroll and pageup
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 1600) {
+      $('.pageup').fadeIn('slow');
+    } else {
+      $('.pageup').fadeOut('slow');
+    }
+  });
+  $(document).ready(function () {
+    $('a[href*=#]').bind('click', function (e) {
+      var anchor = $(this);
+      $('html, body')
+        .stop()
+        .animate(
+          {
+            scrollTop: $(anchor.attr('href')).offset().top,
+          },
+          1000
+        );
+      e.preventDefault();
+    });
+    return false;
+  });
+  new WOW().init();
 });
---
+++ e
@@ -0,0 +1,160 @@
+[33mcommit 54d005dad7c278f675b7b96b06ed54b757690850[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m)[m
+Merge: 0e1eb57 9b09ffc
+Author: Anastasiya <malova.iteb@gmail.com>
+Date:   Wed Jan 26 00:02:40 2022 +0300
+
+    Merge branch 'master' of https://github.com/AnastasiyaMalova/AnastasiyaMalova.github.io into master
+
+[33mcommit 0e1eb57366c8b771075990731d7e47b4a93ff5f1[m
+Author: Anastasiya <malova.iteb@gmail.com>
+Date:   Tue Jan 25 23:59:58 2022 +0300
+
+    change
+
+[33mcommit 9b09ffcc5629eb4c8ddff658f0c141eba949285f[m[33m ([m[1;31morigin/master[m[33m, [m[1;31morigin/HEAD[m[33m)[m
+Author: AnastasiyaMalova <43109290+AnastasiyaMalova@users.noreply.github.com>
+Date:   Thu Dec 23 14:35:00 2021 +0300
+
+    Update README.md
+
+[33mcommit 09bbd39f18e1d5a87b148c7b65efd1db7522c787[m
+Author: AnastasiyaMalova <43109290+AnastasiyaMalova@users.noreply.github.com>
+Date:   Thu Dec 23 14:34:16 2021 +0300
+
+    Create README.md
+
+[33mcommit b7757715f8e13bab40efc33e0ee0da2ed161ec0a[m
+Author: AnastasiyaMalova <43109290+AnastasiyaMalova@users.noreply.github.com>
+Date:   Thu Dec 23 14:33:51 2021 +0300
+
+    Update README.md
+
+[33mcommit 28ee00450bb85a3d2ae8dfb811788aa9aca7b264[m
+Author: AnastasiyaMalova <43109290+AnastasiyaMalova@users.noreply.github.com>
+Date:   Thu Dec 23 14:33:24 2021 +0300
+
+    Update README.md
+
+[33mcommit dc894833a9133c154c0c4a52d01e5d4b1171b51d[m
+Author: AnastasiyaMalova <43109290+AnastasiyaMalova@users.noreply.github.com>
+Date:   Thu Dec 23 14:30:40 2021 +0300
+
+    Update README.md
+
+[33mcommit e5c0e18bcea1f92ef30b36e4b74b64fb02385633[m
+Author: AnastasiyaMalova <43109290+AnastasiyaMalova@users.noreply.github.com>
+Date:   Thu Dec 23 14:30:12 2021 +0300
+
+    Update README.md
+
+[33mcommit f2647064393e01353cdf470d6ca0c8c6e77073a7[m
+Author: Anastasiya <malova.iteb@gmail.com>
+Date:   Thu Dec 23 14:27:52 2021 +0300
+
+    add thousand sunny
+
+[33mcommit be5a1884fe484cf943efbccae5826e5e4b79979d[m
+Author: Anastasiya <malova.iteb@gmail.com>
+Date:   Wed Nov 24 20:09:42 2021 +0300
+
+    Revert "add main.js"
+    
+    This reverts commit fc74ac909b33dc3294f9bb9651c92aba550981ba.
+
+[33mcommit fc74ac909b33dc3294f9bb9651c92aba550981ba[m
+Author: Anastasiya <malova.iteb@gmail.com>
+Date:   Wed Nov 24 20:06:19 2021 +0300
+
+    add main.js
+
+[33mcommit d79af049458e68b8a08fcdbadcc0314397970069[m
+Author: AnastasiyaMalova <43109290+AnastasiyaMalova@users.noreply.github.com>
+Date:   Wed Nov 24 19:55:57 2021 +0300
+
+    Update README.md
+
+[33mcommit e294efdbd2dd00cf78e8514d75892ab6d1564533[m
+Author: AnastasiyaMalova <43109290+AnastasiyaMalova@users.noreply.github.com>
+Date:   Wed Nov 24 19:54:40 2021 +0300
+
+    Create README.md
+    
+    add links for projects
+
+[33mcommit c7a921438136fc484f4fd705d251bf6ed32978d7[m
+Author: Anastasiya <malova.iteb@gmail.com>
+Date:   Wed Nov 24 19:34:33 2021 +0300
+
+    add projects
+
+[33mcommit e780f4d41f1aadaf7beec7e15f4ca7a2cbbc5eb9[m
+Author: AnastasiyaMalova <43109290+AnastasiyaMalova@users.noreply.github.com>
+Date:   Wed Nov 24 19:13:02 2021 +0300
+
+    Create read.me
+
+[33mcommit d806b0f74f1268590d216ea5388d60007f757dc8[m
+Author: AnastasiyaMalova <43109290+AnastasiyaMalova@users.noreply.github.com>
+Date:   Wed Nov 24 18:56:40 2021 +0300
+
+    Delete demo-files directory
+
+[33mcommit 8556f2e7c1a1244a429edc8922e59d3779780d3b[m
+Author: Anastasiya <malova.iteb@gmail.com>
+Date:   Wed Oct 27 23:59:58 2021 +0300
+
+    project created, added files
+
+[33mcommit 800615eb6e601d603bbea34a8a05b012a51aca10[m
+Author: Anastasiya <malova.iteb@gmail.com>
+Date:   Mon Oct 25 15:40:49 2021 +0300
+
+    2
+
+[33mcommit 5db83d3e7267c603b1f879e80af052ce12d2f51f[m
+Author: Anastasiya <malova.iteb@gmail.com>
+Date:   Mon Oct 25 15:38:25 2021 +0300
+
+    add style
+
+[33mcommit 92618f5e9f89f03522ab6d8cf699ec118925096a[m
+Author: Anastasiya <malova.iteb@gmail.com>
+Date:   Mon Oct 25 12:37:12 2021 +0300
+
+    all
+
+[33mcommit 70b6f35b5e49a36281fd82394d0bfe4f66af1a5b[m
+Author: Anastasiya <malova.iteb@gmail.com>
+Date:   Mon Oct 25 11:13:04 2021 +0300
+
+    css
+
+[33mcommit 5b447379b3b3781e975d9847cd10bf9d10d3f127[m
+Author: Anastasiya <malova.iteb@gmail.com>
+Date:   Sun Oct 24 22:51:47 2021 +0300
+
+    style modify
+
+[33mcommit 1ea76882e0d4ce4e0584e67bb6d66bda523b8077[m
+Author: Anastasiya <malova.iteb@gmail.com>
+Date:   Sun Oct 24 22:32:03 2021 +0300
+
+    subheader_btn
+
+[33mcommit 9bfd949dfd33d758f7993795b9a87f352e3ae3e0[m
+Author: Anastasiya <malova.iteb@gmail.com>
+Date:   Sun Oct 24 21:59:29 2021 +0300
+
+    add folder
+
+[33mcommit 7a31cb45a9eba2911d9766b6d1573a2f58ea3321[m
+Author: Anastasiya <malova.iteb@gmail.com>
+Date:   Sun Oct 24 16:03:20 2021 +0300
+
+    subheader_btn is moved
+
+[33mcommit a837b75974b36db090f67f595a97fb20cca3d1fa[m
+Author: Anastasiya <malova.iteb@gmail.com>
+Date:   Fri Sep 10 23:49:00 2021 +0300
+
+    first_commit
